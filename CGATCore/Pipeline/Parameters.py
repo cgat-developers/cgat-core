21fd5bf70622591ca3cba3a814bdb5a2ca53fdad
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2018-03-27 15:45:07 +0100

    Revert "Merge pull request #20 from cgat-developers/{IS}_paramerter_parsing"
    This reverts commit 41fa74ef219c6e910a0ec0ec67935fe471bf9877, reversing
    changes made to e5d2e5a78da031e84930dc82fdcd1c72482e4834.

--- CGATCore/Pipeline/Parameters.py
+++ CGATCore/Pipeline/Parameters.py
@@ -15,8 +15,6 @@ import configparser
 import getpass
 import logging
 import yaml
-import re
-from collections import defaultdict
 
 import CGATCore.Experiment as E
 import CGATCore.IOTools as IOTools
@@ -53,7 +51,7 @@ class TriggeredDefaultFactory:
 # Global variable for parameter interpolation in commands
 # This is a dictionary that can be switched between defaultdict
 # and normal dict behaviour.
-PARAMS = defaultdict(TriggeredDefaultFactory())
+PARAMS = collections.defaultdict(TriggeredDefaultFactory())
 
 # patch - if --help or -h in command line arguments,
 # switch to a default dict to avoid missing paramater
@@ -137,7 +135,7 @@ def config_to_dictionary(config):
         A dictionary of configuration values
 
     """
-    p = defaultdict(lambda: defaultdict(TriggeredDefaultFactory()))
+    p = {}
     for section in config.sections():
         for key, value in config.items(section):
             try:
@@ -149,10 +147,6 @@ def config_to_dictionary(config):
                 raise
 
             p["%s_%s" % (section, key)] = v
-
-            # IMS: new heirarchical format
-            p[section][key] = v
-
             if section in ("general", "DEFAULT"):
                 p["%s" % (key)] = v
 
@@ -160,19 +154,6 @@ def config_to_dictionary(config):
         p["%s" % (key)] = IOTools.str2val(value)
 
     return p
-
-
-def nested_update(old, new):
-    '''Update potentially nested dictionaries. If both old[x] and new[x]
-    inherit from collections.Mapping, then update old[x] with entries from
-    new[x], otherwise set old[x] to new[x]'''
-
-    for key, value in new.items():
-        if isinstance(value, collections.Mapping) and \
-           isinstance(old.get(key, str()), collections.Mapping):
-            old[key].update(new[key])
-        else:
-            old[key] = new[key]
 
 
 def input_validation(PARAMS, pipeline_script=""):
@@ -324,9 +305,9 @@ def get_parameters(filenames=None,
     filenames = [x.strip() for x in filenames]
 
     # update with hard-coded PARAMS
-    nested_update(PARAMS, HARDCODED_PARAMS)
+    PARAMS.update(HARDCODED_PARAMS)
     if defaults:
-        nested_update(PARAMS, defaults)
+        PARAMS.update(defaults)
 
     # reset working directory. Set in PARAMS to prevent repeated calls to
     # os.getcwd() failing if network is busy
@@ -342,7 +323,6 @@ def get_parameters(filenames=None,
     yml_filenames = [x for x in filenames if not x.endswith(".ini")]
 
     if ini_filenames:
-
         conf = configparser.SafeConfigParser()
         try:
             conf.read(ini_filenames)
@@ -358,9 +338,8 @@ def get_parameters(filenames=None,
             config = configparser.RawConfigParser()
             config.read(ini_filenames)
             p = config_to_dictionary(config)
-
         if p:
-            nested_update(PARAMS, p)
+            PARAMS.update(p)
 
     if yml_filenames:
         for filename in yml_filenames:
@@ -372,7 +351,7 @@ def get_parameters(filenames=None,
             with open(filename) as inf:
                 p = yaml.load(inf)
                 if p:
-                    nested_update(PARAMS, p)
+                    PARAMS.update(p)
 
     # for backwards compatibility - normalize dictionaries
     p = {}
@@ -380,7 +359,7 @@ def get_parameters(filenames=None,
         if isinstance(v, collections.Mapping):
             for kk, vv in v.items():
                 p["{}_{}".format(k, kk)] = vv
-    nested_update(PARAMS, p)
+    PARAMS.update(p)
                 
     # interpolate some params with other parameters
     for param in INTERPOLATE_PARAMS:
