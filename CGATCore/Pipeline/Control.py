5dcc73c285902bbbd35ecdcd0f7e051e19c7b5b3
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2018-05-11 17:06:08 +0100

    try aborting when DRMAA is not available and no --local option is specified

--- CGATCore/Pipeline/Control.py
+++ CGATCore/Pipeline/Control.py
@@ -1129,6 +1129,12 @@ def run_workflow(options, args, pipeline=None):
                     messenger = LoggingFilterProgress(stream.getvalue())
                     logger.addFilter(messenger)
 
+                    if not options.without_cluster and not HAS_DRMAA:
+                        E.critical("DRMAA API not found so cannot talk to a cluster.")
+                        E.critical("Please use --local to run the pipeline"
+                                   " on this host: {}".format(os.uname()[1]))
+                        sys.exit(-1)
+
                     global task
                     if options.without_cluster:
                         # use ThreadPool to avoid taking multiple CPU for pipeline
