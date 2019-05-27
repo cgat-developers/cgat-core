472271068e4db2ef39f7e1f67ea3e7a9adb916d9
Adam Cribbs <adam.cribbs@imm.ox.ac.uk>
2019-05-27 08:53:50 +0100

    upload function of raws

--- remotes/AWS.py
+++ remotes/AWS.py
@@ -58,10 +58,33 @@ class S3Connection():
                             file or path is specified. It currently is: {}'''.format(dest_path))
 
     def remote_upload(self,
-                        bucket_name,
-                        file_dir):
+                      bucket_name,
+                      file_dir,
+                      key=None):
         '''Upload data/file to an S3 bucket.'''
 
+        file_path = os.path.realpath(os.path.expanduser(file_dir))
+
+        if not bucket_name:
+            raise ValueError("Bucket name must be specified to upload file")
+        if not os.path.exists(file_dir):
+            raise ValueError("File path specified does not exitis: {}".format(file_path))
+        if not ps.path.isfile(file_dir):
+            raise ValueError("File path specified is not a file: {}".format(file_path))
+
+        if not self.bucket_exists(bucket_name):
+            self.S3.create_bucket(Bucket=bucket_name) # Implement other features fuch as CreateBucketConfiguration
+
+        f = self.S3.Object(bucket_name, key)
+
+        try:
+            f.upload_file(file_dir, bucket_name, key)
+        except:
+            raise Exception("filename is not correctly specified: {}".format(file_dir))
+
+        return file_dir
+
+        
     def delete_object_bucket(self,
                                bucket_name,
                                key):
