df42602a67b769c5bda87551125c6ef4d703a77b
Acribbs <adam.cribbs@imm.ox.ac.uk>
2019-06-01 10:56:35 +0100

    updated bucket exists functions

--- cgatcore/remote/aws.py
+++ cgatcore/remote/aws.py
@@ -18,11 +18,17 @@ class S3RemoteObject(AbstractRemoteObject):
 
         self._S3object = S3Connection(*args, **kwargs)
 
-    def exists(self):
-        if self._matched_s3_path:
-            return self._S3object.exists_in_bucket(self.bucket_name, self.key)
-        else:
-            raise S3FileException("The file cannot be parsed as an s3 path in form 'bucket/key': %s" % self.local_file())
+    def exists(self, bucket_name, key):
+        
+        try:
+            self._S3object.bucket_exists(bucket_name)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                return False
+            else:
+                raise S3FileException("The file cannot be parsed as an s3 path in form 'bucket/key': %s" % self.local_file())
+
+        return True
 
     def download(self, bucket_name, key, file_dir):
         self._S3object.remote_download(bucket_name, key, file_dir)
