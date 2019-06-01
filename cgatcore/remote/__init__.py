c2995e6edcddd728efa38b7a476680e6d15cec56
Acribbs <adam.cribbs@imm.ox.ac.uk>
2019-06-01 07:09:21 +0100

    added abstract class object

--- cgatcore/remote/__init__.py
+++ cgatcore/remote/__init__.py
@@ -1,1 +1,29 @@
-from cgatcore.remote.aws import *
+import os
+import sys
+from abc import abstractmethod
+
+class AbstractRemoteObject():
+    '''This is an abstract class that all RemoteObjects will
+       inherit from. This is an abstract class to ridgidly define
+       the abstract methods of this RemoteObject class'''
+
+    def __init__(self, *args, **kwargs):
+
+        self.args = args
+        self.kwargs = kwargs
+
+    @abstractmethod
+    def exists(self):
+        pass
+
+    @abstractmethod
+    def download(self):
+        pass
+
+    @abstractmethod
+    def upload(self):
+        pass
+
+    @abstractmethod
+    def delete_file(self):
+        pass
