8c6f2533acc24b587d13f6f88b5515d1b2a44514
Andreas Heger <andreas.heger@gmail.com>
2019-11-19 21:00:58 +0000

    {AH} fix tests for py3.7

--- tests/test_pipeline_cli.py
+++ tests/test_pipeline_cli.py
@@ -7,17 +7,26 @@ import pytest
 mock = Mock()
 cgatcore.pipeline.execution.GLOBAL_SESSION = mock()
 
+# python 3.7 vs 3.8 difference
+def get_options(obj):
+    args = obj.call_args.args
+    if isinstance(args, dict):
+        return args
+    else:
+        return list(obj.call_args)[0][0]
+
+
 
 @patch.object(GridExecutor, "setup_job")
 def test_default_queue_arguments(grid_run_patch):
-    P.initialize()
+    P.initialize(argv=["mytool"])
     with patch("cgatcore.pipeline.execution.will_run_on_cluster", return_value=True):
         # fails with NameError if drmaa not configured
         # and import drmaa has failed
         with pytest.raises(NameError):
             P.run("echo here")
         grid_run_patch.assert_called_once()
-        options = grid_run_patch.call_args_list[0].args[0]
+        options = get_options(grid_run_patch)
         assert options["queue"] == "all.q"
         assert options["queue_manager"] == "sge"
 
@@ -31,7 +40,7 @@ def test_default_queue_can_be_overridden(grid_run_patch):
         with pytest.raises(NameError):
             P.run("echo here")
         grid_run_patch.assert_called_once()
-        options = grid_run_patch.call_args_list[0].args[0]
+        options = get_options(grid_run_patch)
         assert options["queue"] == "test.q"
         assert options["queue_manager"] == "sge"
 
@@ -54,6 +63,6 @@ def test_all_cluster_parameters_can_be_set(grid_run_patch, option, field, value)
         with pytest.raises(NameError):
             P.run("echo here")
         grid_run_patch.assert_called_once()
-        options = grid_run_patch.call_args_list[0].args[0]
+        options = get_options(grid_run_patch)
         assert options[field] == value
         
