4f36da48f913b770d34c643d2e002ae1d8dfce98
Acribbs <a.p.cribbs@gmail.com>
2024-10-29 14:42:43 +0000

    avoid static imports

--- cgatcore/__init__.py
+++ cgatcore/__init__.py
@@ -1,2 +1,5 @@
-from cgatcore import pipeline
-from cgatcore import remote
+# cgatcore/__init__.py
+import importlib
+
+pipeline = importlib.import_module('cgatcore.pipeline')
+remote = importlib.import_module('cgatcore.remote')
